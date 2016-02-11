9984889320c4f5d247cc4b518d161c9503b382f3
Ricardo Ruiz <ralmendro@ciandt.com>
2016-02-11 15:08:09 -0200

    Refatorando para usar PageObjects nos testes e2e

--- test/e2e/contatosPageSpec.js
+++ test/e2e/contatosPageSpec.js
@@ -1,22 +1,23 @@
+var ContatosPage = new require("./pages/contatosPage");
+
 describe("Página principal", function(){
    
+   var pagina = new ContatosPage();
+   
    beforeEach(function(){
-      browser.get("http://localhost:3000/#/contatos"); 
+      pagina.visitar(); 
    });
    
-   it("Deve estar logado", function(){
-       element(by.id("usuario-logado")).getText().then(function(texto){
+   it("Deve estar logado", function(){       
+       pagina.obterUsuarioLogado().then(function(texto){
           expect(texto.trim().length).toBeGreaterThan(0); 
        });
    });
    
    it("Deve remover um contato da lista", function(){
-       var totalAntes = element.all(by.repeater("contato in contatos")).count();
-       
-       element(by.repeater("contato in contatos").row(0)).element(by.css(".btn")).click();
-       
-       var totalDepois = element.all(by.repeater("contato in contatos")).count();
-       
+       var totalAntes = pagina.obterTotalDeItensDaLista();
+       pagina.removerPrimeiroItemDaLista();
+       var totalDepois = pagina.obterTotalDeItensDaLista();       
        expect(totalDepois).toBeLessThan(totalAntes);
    }); 
     
